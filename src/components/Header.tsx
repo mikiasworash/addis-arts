5ef26a852ce7e29e893ff1cc3813e80307519f81
Mikias Worash <mikiasworash@gmail.com>
2024-02-15 12:25:02 +0300

    added home grid

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <header className="fixed top-0 bg-black w-full mx-auto border-b border-b-[#333333]">
+    <header className="fixed top-0 bg-black w-full mx-auto border-b border-b-[#333333] z-50">
       <div className="max-w-fit md:max-w-screen-xl mx-auto p-4 md:flex md:items-center md:justify-between">
         <span className="text-sm text-gray-400 sm:text-center">
           <Link to="/" className="text-white hover:text-gray-200 text-xl">
@@ -20,7 +20,7 @@ const Header = () => {
           </li>
           <li>
             <Link
-              to="/offers"
+              to="/"
               className="hover:underline underline-offset-4 me-4 md:me-6"
             >
               Offers
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,91 @@
+import { LayoutGrid } from "@/components/ui/layout-grid";
+
 const Home = () => {
   return (
-    <div className="h-screen">
-      <h1>Home Page</h1>
+    <div className="h-screen py-20 w-full -z-10">
+      <LayoutGrid cards={cards} />
     </div>
   );
 };
 export default Home;
+
+const SkeletonOne = () => {
+  return (
+    <div>
+      <p className="font-bold text-4xl text-white">Digital Dreamscape</p>
+      <p className="font-normal text-base text-white"></p>
+      <p className="font-normal text-base my-4 max-w-lg text-neutral-200">
+        A digital art series exploring surreal landscapes and dreamlike scenes,
+        created with cutting-edge digital techniques.
+      </p>
+    </div>
+  );
+};
+
+const SkeletonTwo = () => {
+  return (
+    <div>
+      <p className="font-bold text-4xl text-white">Cinematic Visions</p>
+      <p className="font-normal text-base text-white"></p>
+      <p className="font-normal text-base my-4 max-w-lg text-neutral-200">
+        A series of mixed-media artworks inspired by iconic films and cinematic
+        moments, reimagined through the artist's creative lens.
+      </p>
+    </div>
+  );
+};
+const SkeletonThree = () => {
+  return (
+    <div>
+      <p className="font-bold text-4xl text-white">Urban Rhythms</p>
+      <p className="font-normal text-base text-white"></p>
+      <p className="font-normal text-base my-4 max-w-lg text-neutral-200">
+        A series of photography prints showcasing the bustling streets and
+        architectural marvels of modern cities.
+      </p>
+    </div>
+  );
+};
+const SkeletonFour = () => {
+  return (
+    <div>
+      <p className="font-bold text-4xl text-white">Echoes of the Past</p>
+      <p className="font-normal text-base text-white"></p>
+      <p className="font-normal text-base my-4 max-w-lg text-neutral-200">
+        Artworks breathe life into the pages of history, sparking curiosity and
+        contemplation in those who behold them.
+      </p>
+    </div>
+  );
+};
+
+const cards = [
+  {
+    id: 1,
+    content: <SkeletonOne />,
+    className: "md:col-span-2",
+    thumbnail:
+      "https://source.unsplash.com/blue-black-and-orange-abstract-painting-3Sf_G9m0gcQ",
+  },
+  {
+    id: 2,
+    content: <SkeletonTwo />,
+    className: "col-span-1",
+    thumbnail:
+      "https://source.unsplash.com/water-droplets-on-glass-panel-wzkv5p5CIRk",
+  },
+  {
+    id: 3,
+    content: <SkeletonThree />,
+    className: "col-span-1",
+    thumbnail:
+      "https://source.unsplash.com/floral-persons-portrait-graffiti-fT49QnFucQ8",
+  },
+  {
+    id: 4,
+    content: <SkeletonFour />,
+    className: "md:col-span-2",
+    thumbnail:
+      "https://source.unsplash.com/a-painting-of-a-group-of-men-on-horses-5EUh-tq31eA",
+  },
+];
